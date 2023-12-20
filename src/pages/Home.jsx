522f4a5eb54871d5db6dd4ddf52f2f5a70ddc5e5
Deveater0072 <maszynistanaczas@gmail.com>
2023-12-20 21:42:11 +0100

    more changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,48 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-// import Pagination from "@mui/material/Pagination";
-// import Stack from "@mui/material/Stack";
 import Pokemons from "../data/data";
 import PokemonCard from "../components/PokemonCard";
+import { useState } from "react";
+import axios from "axios";
+import { Button, Stack } from "@mui/material";
 
 const Home = () => {
+  const [pokemons, setPokemons] = useState(Pokemons);
+  const [pageNumber, setPageNumber] = useState(1);
+  const getPokemonByName = async (name) => {
+    try {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character?name=${name}`
+      );
+      const data = response.data.results;
+      setPokemons(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onChangeHandler = (event) => {
+    getPokemonByName(event.target.value);
+    console.log(event.target.value);
+  };
+  const onChangePageMinus = () => {
+    // const newPageNumber = pageNumber - 1;
+    // if (newPageNumber > 0) {
+    //   setPageNumber(newPageNumber);
+    // } else {
+    //   setPageNumber(1);
+
+    // }
+    setPageNumber(Math.max(pageNumber - 1, 1));
+  };
+
+  // alternatywa do skroconej wersji linijka 62
+
+  // const onChangePagePlus = () => {
+  //   setPageNumber(pageNumber + 1);
+  // };
+
   return (
     <>
       <Box
@@ -16,15 +53,34 @@ const Home = () => {
         noValidate
         autoComplete="off"
       >
-        <TextField id="outlined-basic" label="Pokemon Name" variant="filled" />
-        {Pokemons.map((pokemon) => (
-          <PokemonCard pokemon={pokemon} />
-        ))}
+        <TextField
+          id="outlined-basic"
+          label="Pokemon Name"
+          variant="filled"
+          onChange={onChangeHandler}
+        />
+        <Button
+          disabled={pageNumber === 1}
+          variant="contained"
+          onClick={onChangePageMinus}
+        >
+          &lt;
+        </Button>
+        {pageNumber}
+        <Button
+          variant="contained"
+          onClick={() => setPageNumber(pageNumber + 1)}
+        >
+          &gt;{" "}
+        </Button>
+        <Stack direction={"row"} spacing={2}>
+          {pokemons.map((pokemon) => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+        </Stack>
       </Box>
     </>
   );
 };
 
 export default Home;
-
-
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,43 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Stack } from "@mui/material";
+
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-        <li>
-            <Link to="/Home">
-              <Button variant="contained">Home</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Favorites">
-              <Button variant="contained">Favorites</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Arena">
-              <Button variant="contained">Arena</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Login">
-              <Button variant="contained">Login</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Registration">
-              <Button variant="contained">Registration</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Logout">
-              <Button variant="contained">Logout</Button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
+      <Stack spacing={1} >
+        <Link to="/Home">
+          <Button variant="contained">Home</Button>
+        </Link>
+        <Link to="/Favorites">
+          <Button variant="contained">Favorites</Button>
+        </Link>
+        <Link to="/Arena">
+          <Button variant="contained">Arena</Button>
+        </Link>
+        <Link to="/Login">
+          <Button variant="contained">Login</Button>
+        </Link>
+        <Link to="/Registration">
+          <Button variant="contained">Registration</Button>
+        </Link>
+        <Link to="/Logout">
+          <Button variant="contained">Logout</Button>
+        </Link>
+      </Stack>
       <Outlet />
     </>
   );
